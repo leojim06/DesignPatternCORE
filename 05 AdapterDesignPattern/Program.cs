9b1240947ffa5defe478880c441d8a60fdde46b5
leojim06 <leojim06@gmail.com>
2019-04-26 15:36:04 -0500

    Creación patrón Adapter

---
+++ 05 AdapterDesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _05_AdapterDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = new XmlConverter();
+            document.GetXML();
+        }
+    }
+}
